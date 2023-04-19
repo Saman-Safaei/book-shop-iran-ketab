bc292aa28ea3e954472f5939f1b19adc78f25cfd
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-19 23:47:51 +0430

    feat: add block option to button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,7 +9,7 @@ const Button: FC<ButtonProps> = ({ className, block, children, ...props }) => {
 
   return (
     <button
-      className={`bg-primary-main border-solid border-2 border-primary-main hover:bg-primary-300 active:bg-primary-500 px-4 py-2 rounded-md text-white ${blockClasses} ${className} transition-colors duration-300`}
+      className={`${blockClasses} bg-primary-main border-solid border-2 border-primary-main hover:bg-primary-200 hover:text-text-main active:bg-primary-400 px-4 py-2 rounded-md text-white ${blockClasses} ${className} transition-colors duration-300`}
       {...props}>
       {children}
     </button>
