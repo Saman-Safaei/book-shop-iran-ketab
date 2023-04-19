229bed89c8ff302c15e2be19c42ae1c1651bf7ae
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-19 22:18:00 +0430

    feat: wrap app with redux provider and create page types for layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import type { NextPage } from 'next';
+import type { ReactElement, ReactNode } from 'react';
+import { useState } from 'react';
+import { Provider } from 'react-redux';
+import createStore from '@/store';
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [store] = useState(() => createStore());
+  const getLayout = Component.getLayout || ((page: ReactNode) => page);
+  
+  return (
+    <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+  );
 }
