d814b3620895561cf617b5f9608fbf383c934e91
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-19 22:28:26 +0430

    feat: add hover effect to link components

--- components/navbar/NavbarLink.tsx
+++ components/navbar/NavbarLink.tsx
@@ -8,10 +8,13 @@ export interface NavbarLinkProps {
 
 const NavbarLink: FC<NavbarLinkProps> = ({ children, href }) => {
   return (
-    <li>
-      <Link className="text-text-main hover:text-primary-main transition-colors duration-300" href={href}>
+    <li className="relative">
+      <Link
+        className="peer text-text-main hover:text-primary-main transition-colors duration-300"
+        href={href}>
         {children}
       </Link>
+      <span className="absolute bottom-[120%] right-1/2 translate-x-1/2 block w-1.5 h-1.5 rounded-full bg-transparent peer-hover:bg-primary-main transition-colors duration-300" />
     </li>
   );
 };
