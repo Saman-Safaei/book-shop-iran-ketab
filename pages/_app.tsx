9ded35accacc45d57d828c65b107eca9caa31c30
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-22 12:56:56 +0430

    feat: import animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import type { AppProps } from 'next/app';
-import type { NextPage } from 'next';
-import type { ReactElement, ReactNode } from 'react';
-import { useState } from 'react';
-import { Provider } from 'react-redux';
-import createStore from '@/store';
-import '@/styles/globals.css';
+import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import type { ReactElement, ReactNode } from "react";
+import { useState } from "react";
+import { Provider } from "react-redux";
+import createStore from "@/store";
+import "@/styles/globals.css";
+import "@/styles/animate-classes.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -17,7 +18,7 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const [store] = useState(() => createStore());
   const getLayout = Component.getLayout || ((page: ReactNode) => page);
-  
+
   return (
     <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
   );
