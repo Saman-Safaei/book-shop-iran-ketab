aab1d9d4b911d3dd15a12e31243ebe7530bd33ea
Saman-Safaei <xsamansafaeix@gmail.com>
2023-04-22 12:59:15 +0430

    feat: make navbar visible when scroll

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,53 +6,72 @@ import logo from "~/logo.png";
 import { Bag, HambergerMenu, Profile } from "iconsax-react";
 import NavbarLink from "@/components/navbar/NavbarLink";
 import Search from "@/components/navbar/Search";
+import { useAppDispatch } from "@/store";
+import { actions } from "@/store/ui-slice";
+import useScrollData from "@/hooks/useScrollData";
+import { Fragment } from "react";
 
 export interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
+  const [biggerThan5] = useScrollData(5);
+  const dispatch = useAppDispatch();
+
+  const navbarClasses = biggerThan5 ? "shadow" : "lg:pt-4";
+
   return (
-    <nav className="lg:pt-4 bg-white">
-      <div className="max-w-7xl mx-auto h-navbar-height flex flex-row items-stretch justify-between px-4">
-        <ul className='flex lg:hidden h-full flex-row items-center'>
-          <li>
-            <button className='block'>
-              <HambergerMenu size="32" color='#FD8F32' />
-            </button>
-          </li>
-        </ul>
-        <ul className="h-full hidden lg:flex flex-row items-center gap-6">
-          <li>
-            <Link href="/">
-              <Image
-                className="h-12 w-12 object-contain"
-                src={logo}
-                alt="logo"
+    <Fragment>
+      <div className="h-navbar-height pt-4" />
+      <nav
+        className={`fixed left-0 right-0 top-0 z-40 bg-white/90 ${navbarClasses} backdrop-blur-md transition-[padding] duration-300`}>
+        <div className="mx-auto flex h-navbar-height max-w-7xl flex-row items-stretch justify-between px-4">
+          <ul className="flex h-full flex-row items-center lg:hidden">
+            <li>
+              <button
+                onClick={() => dispatch(actions.toggleSidebar())}
+                className="block">
+                <HambergerMenu size="32" color="#FD8F32" />
+              </button>
+            </li>
+          </ul>
+          <ul className="hidden h-full flex-row items-center gap-6 lg:flex">
+            <li>
+              <Link href="/">
+                <Image
+                  className="h-12 w-12 object-contain"
+                  src={logo}
+                  alt="logo"
+                />
+              </Link>
+            </li>
+            <NavbarLink href="/">خانه</NavbarLink>
+            <NavbarLink href="/">بزرگسال</NavbarLink>
+            <NavbarLink href="/">نوجوان</NavbarLink>
+            <NavbarLink href="/">کودک</NavbarLink>
+            <NavbarLink href="/">درباره ما</NavbarLink>
+            <NavbarLink href="/">تماس با ما</NavbarLink>
+          </ul>
+          <ul className="flex h-full flex-row-reverse items-center gap-[10px]">
+            <li className="block lg:hidden">
+              <Image className="h-8 w-8" src={logo} alt="logo" />
+            </li>
+            <li>
+              <Bag size="30" className="text-primary-main" variant="Linear" />
+            </li>
+            <li className="hidden lg:block">
+              <Profile
+                size="30"
+                className="text-primary-main"
+                variant="Linear"
               />
-            </Link>
-          </li>
-          <NavbarLink href="/">خانه</NavbarLink>
-          <NavbarLink href="/">بزرگسال</NavbarLink>
-          <NavbarLink href="/">نوجوان</NavbarLink>
-          <NavbarLink href="/">کودک</NavbarLink>
-          <NavbarLink href="/">درباره ما</NavbarLink>
-          <NavbarLink href="/">تماس با ما</NavbarLink>
-        </ul>
-        <ul className="flex flex-row-reverse h-full items-center gap-[10px]">
-          <li className='block lg:hidden'>
-            <Image className='h-8 w-8' src={logo} alt='logo' />
-          </li>
-          <li>
-            <Bag size="30" className="text-primary-main" variant="Linear" />
-          </li>
-          <li className="hidden lg:block">
-            <Profile size="30" className="text-primary-main" variant="Linear" />
-          </li>
-          <li>
-            <Search />
-          </li>
-        </ul>
-      </div>
-    </nav>
+            </li>
+            <li>
+              <Search />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
   );
 };
 
