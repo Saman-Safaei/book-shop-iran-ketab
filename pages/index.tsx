42b108b686df52e67a12e613117906bcfb99cedc
Saman-Safaei <xsamansafaeix@gmail.com>
2023-05-05 14:29:44 +0430

    feat: add fake data to swiper slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { Fragment } from "react";
 import MasterLayout from "@/components/layout/MasterLayout";
 import BooksSection from "@/components/book-slider/BooksSection";
 import { Crown, Flash } from "iconsax-react";
+
 import books from "@/assets/fake-data/new-books.json";
+import mostSellBooks from '@/assets/fake-data/most-sell-books.json';
 
 export interface HomePageProps {}
 
@@ -20,7 +22,7 @@ const HomePage: NextPageWithLayout<HomePageProps> = () => {
         title="پر فروش ترین ها"
         category="all"
         icon={<Crown size="24" />}
-        products={books}
+        products={mostSellBooks}
       />
     </Fragment>
   );
